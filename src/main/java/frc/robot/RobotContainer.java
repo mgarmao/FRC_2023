60d96af17b47158348635d1d616c20ae1e751b4e
mgarmao <mgarmao830@gmail.com>
2023-01-26 17:02:00 -0500

    Photon Vision WORKING!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
+import org.opencv.photo.Photo;
+
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
--- src/main/java/frc/robot/subsystems/Photon.java
+++ src/main/java/frc/robot/subsystems/Photon.java
@@ -3,18 +3,16 @@ package frc.robot.subsystems;
 import java.util.List;
 
 import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Photon extends SubsystemBase{
-    PhotonCamera camera = new PhotonCamera("Limelight 2");//or photonvision
-    // Angle between horizontal and the camera.
+    PhotonCamera camera = new PhotonCamera("OV5647");//or photonvision
+//     // Angle between horizontal and the camera.
     final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(0);    
     double CAMERA_HEIGHT_METERS = 1;
     double TARGET_HEIGHT_METERS =1;
@@ -23,53 +21,61 @@ public class Photon extends SubsystemBase{
     double kTargetHeight = 1;
     double kCameraPitch = 1;
     double kTargetPitch = 1;
-    // AprilTagFieldLayout aprilTagFieldLayout = new AprilTagFieldLayout(AprilTagFieldLayout.loadFromResource(AprilTagFields.k2022RapidReact.m_resourceFile));
+//     // AprilTagFieldLayout aprilTagFieldLayout = new AprilTagFieldLayout(AprilTagFieldLayout.loadFromResource(AprilTagFields.k2022RapidReact.m_resourceFile));
 
     public Photon() {
+        SmartDashboard.putNumber("Photon", 0);
     }
 
     @Override
     public void periodic() {
-        var result = camera.getLatestResult();
-
+        var result = camera.getLatestResult();         
         boolean hasTargets = result.hasTargets();
         List<PhotonTrackedTarget> targets = result.getTargets();
-        PhotonTrackedTarget target = result.getBestTarget();
-        double yaw = target.getYaw();
-        double pitch = target.getPitch();
-        double area = target.getArea();
-        double skew = target.getSkew();
-        int id = target.getFiducialId();
-        double poseAmb = target.getPoseAmbiguity();
-        Transform3d targetToCamera = target.getBestCameraToTarget();
+
+        SmartDashboard.putBoolean("Has Target",hasTargets);
+        if(hasTargets){
+            PhotonTrackedTarget target = result.getBestTarget();
+            double yaw = target.getYaw();
+            double pitch = target.getPitch();
+            double area = target.getArea();
+            double skew = target.getSkew();
+            int id = target.getFiducialId();
+            double poseAmb = target.getPoseAmbiguity();
+            Transform3d targetToCamera = target.getBestCameraToTarget();
+            
+            SmartDashboard.putNumber("yaw",yaw);
+            SmartDashboard.putNumber("pitch",pitch);
+            SmartDashboard.putNumber("area",area);
+            SmartDashboard.putNumber("skew",skew);
+            SmartDashboard.putNumber("id",id);
+            SmartDashboard.putNumber("poseAmb",poseAmb);
+            SmartDashboard.putNumber("targetToCamera X",targetToCamera.getX());
+            SmartDashboard.putNumber("targetToCamera Y",targetToCamera.getY());
+            SmartDashboard.putNumber("targetToCamera Z",targetToCamera.getZ());
+
+        }
         
-        SmartDashboard.putBoolean("Has Target", hasTargets);
-        SmartDashboard.putNumber("yaw",yaw);
-        SmartDashboard.putNumber("pitch",pitch);
-        SmartDashboard.putNumber("area",area);
-        SmartDashboard.putNumber("skew",skew);
-        SmartDashboard.putNumber("id",id);
-        SmartDashboard.putNumber("poseAmb",poseAmb);
-        SmartDashboard.putNumber("targetToCamera X",targetToCamera.getX());
-        SmartDashboard.putNumber("targetToCamera Y",targetToCamera.getY());
-        SmartDashboard.putNumber("targetToCamera Z",targetToCamera.getZ());
+        
 
-        // double distanceToTarget = PhotonUtils.getDistanceToPose(robotPose, targetPose);
-    }
 
-    public void autoAline(){
-        // var result = camera.getLatestResult();
-        // PhotonTrackedTarget target = result.getBestTarget();
-
-        // double range = PhotonUtils.calculateDistanceToTargetMeters(
-        //     CAMERA_HEIGHT_METERS,
-        //     TARGET_HEIGHT_METERS,
-        //     CAMERA_PITCH_RADIANS,
-        //     Units.degreesToRadians(result.getBestTarget().getPitch()));
-
-        // Pose2D robotPose = PhotonUtils.estimateFieldToRobot(kCameraHeight, kTargetHeight, kCameraPitch, kTargetPitch, Rotation2d.fromDegrees(-target.getYaw()), gyro.getRotation2d(), targetPose, cameraToRobot);
-        // double forwardSpeed = -controller.calculate(range, 2);
-        // double distanceToTarget = PhotonUtils.getDistanceToPose(robotPose, targetPose);
+        
+//         // double distanceToTarget = PhotonUtils.getDistanceToPose(robotPose, targetPose);
 
     }
+//     // public void autoAline(){
+//     //     var result = camera.getLatestResult();
+//     //     PhotonTrackedTarget target = result.getBestTarget();
+
+//     //     double range = PhotonUtils.calculateDistanceToTargetMeters(
+//     //         CAMERA_HEIGHT_METERS,
+//     //         TARGET_HEIGHT_METERS,
+//     //         CAMERA_PITCH_RADIANS,
+//     //         Units.degreesToRadians(result.getBestTarget().getPitch()));
+
+//     //     Pose2D robotPose = PhotonUtils.estimateFieldToRobot(kCameraHeight, kTargetHeight, kCameraPitch, kTargetPitch, Rotation2d.fromDegrees(-target.getYaw()), gyro.getRotation2d(), targetPose, cameraToRobot);
+//     //     double forwardSpeed = -controller.calculate(range, 2);
+//     //     double distanceToTarget = PhotonUtils.getDistanceToPose(robotPose, targetPose);
+
+//     // }
 }

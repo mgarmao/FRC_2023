22994d3d9659caba913323f2002e5ffe98db1413
saahirvazirani <rhinohogsv@gmail.com>
2023-02-03 16:26:40 -0500

    Editing Rotate

--- src/main/java/frc/robot/commands/Autonomous/TwoBall.java
+++ src/main/java/frc/robot/commands/Autonomous/TwoBall.java
@@ -19,35 +19,7 @@ import frc.robot.commands.Shooter.*;
 public class TwoBall extends SequentialCommandGroup {
     public TwoBall() {
       addCommands(
-        new InstantCommand(m_intake::extend, m_intake),
-        new TankDrive(m_drivetrain, () -> 0.65, () -> 0.65).alongWith(
-          new InstantCommand(m_intake::retrieve, m_intake),
-          new IndexerController(m_indexer, () -> -0.75),
-          new InstantCommand(m_gatekeeper::manual, m_gatekeeper)
-        ).withTimeout(1.5),
-        new InstantCommand(m_gatekeeper::stop, m_gatekeeper).alongWith(
-          new InstantCommand(m_intake::stop, m_intake)
-        ),
-        new InstantCommand(m_intake::stop, m_intake),
-        new InstantCommand(m_intake::retract, m_intake),
-        new TankDrive(m_drivetrain, () -> -0.65, () -> -0.65).withTimeout(1.5),
-        new Rotate(180).withTimeout(1),
-        new TankDrive(m_drivetrain, () -> 0.65, () -> 0.65).withTimeout(1.5),
-
-        new SequentialCommandGroup(
-        new ShooterController(m_shooter, () -> 1.0).withTimeout(5)).alongWith(
-          new GatekeeperController(m_gatekeeper, () -> 1.0).withTimeout(0.7),
-          new IndexerController(m_indexer, () -> -1.0).withTimeout(2)
-        ).withTimeout(0.7),
-
-        new ShooterController(m_shooter, () -> 1.0).withTimeout(5).alongWith(
-          new GatekeeperController(m_gatekeeper, () -> 1.0).withTimeout(0.7),
-          new IndexerController(m_indexer, () -> -1.0).withTimeout(2)
-        ).withTimeout(0.7),
-
-        new TankDrive(m_drivetrain, () -> -0.65, () -> -0.65).withTimeout(0.8),
-        new Rotate(180).withTimeout(1),
-        new InstantCommand(m_intake::retrieve, m_intake)
+        new Rotate(90)
       );
   }
 }
